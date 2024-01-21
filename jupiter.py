28383ee3049fdab6b2e071c3e63e0d0f35b9cd1b
Hans <tan.wenhans@gmail.com>
2024-01-21 18:34:10 +0000

    Completed Task 2

--- jupiter.py
+++ jupiter.py
@@ -3,11 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 class Moons:
 	def __init__(self,db_path):
 		self.data = pd.DataFrame()
 		self.load_data(db_path)
+		self.X_test = None
+		self.y_test = None
 
 	def load_data(self,db_path):
 		connect = sqlite3.connect(db_path)
@@ -108,24 +115,27 @@ class Moons:
 
 		return new_instance
 
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from skllearn.metrics import mean_squared _error
-import matplotlib.pyplot as plt
-import numpy as np
-
+	#Regression Model to predict values
 	def linear_regression_model(self,test_size):
-		#Calculate the x-values, y-values
-		self.data['T-squared'] = self.data['period_days']**2
-		self.data['a_cubed'] = self.data['distance_km']**3
+		#Convertions
+		self.data['period_seconds'] = self.data['period_days'] *24 *60*60
+		self.data['distance_metres'] = self.data['distance_km'] * 1000
 
-		#Give clear variable names to the values
+		#Calculate the x-values, y-values
+		self.data['T_squared'] = self.data['period_seconds']**2
+		self.data['a_cubed'] = self.data['distance_metres']**3
+
+		#Give clear variable names
 		X = self.data[['T_squared']]
 		y= self.data['a_cubed']
 
 		#We split the data for training and testing.
 		X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=test_size, random_state=42)
 
+		#adding test data as an attribute
+		self.X_test, self.y_test = X_test, y_test
+
+		#Linear Regression Model
 		model = LinearRegression()
 
 		#Train the Model
@@ -133,24 +143,55 @@ import numpy as np
 		#Testing the model
 		y_pred = model.predict(X_test)
 
-		#mean-squared-error for the model - how accurate is it?
-		mse = mean_squared_erro(y_test,y_pred)
+		#Values to determine accuracy of the model
+		mse = mean_squared_error(y_test,y_pred)
+		r2 = r2_score(y_test,y_pred)
+
+		print(f"The Mean Squared Error is {mse}")
+		print(f"The R-square is {r2}")
+
+		#Model Parameters
+		print(f"Line gradient from model: {model.coef_[0]}")
+		print(f"Line intercept from model: {model.intercept_}")
 
 		#Labels given for the graph
-		plt.scatter(X_test,y_test, color='blue')
-		plt.plot(X_test, y_pred, color="black")
+		fig, ax =plt.subplots()
+		ax.scatter(X_test,y_test, color='blue', label="Actual data")
+		ax.plot(X_test, y_pred, color="black", label="Predicted data")
 		plt.title('Linear Regression Model')
-		plt.xlabel('T^2')
-		plt.ylabel('a^3')
-		plt.show()
+		ax.set_xlabel('T^2')
+		ax.set_ylabel('a^3')
+		ax.legend()
 
-		#here, we return the model 
+		#here, we return the model
 		return model
 
+	#checking difference between predicted and actual values
+	def plot_residuals(self,model):
+		y_pred = model.predict(self.X_test)
+		residuals = self.y_test - y_pred
+
+		#plotting the graph
+		plt.scatter(self.X_test,residuals,color="orange",alpha=0.5)
+		plt.axhline(y=0,color="black", linestyle='--')
+		plt.title("Residuals Plot")
+		plt.xlabel("Predicted Values")
+		plt.ylabel("Residuals")
+		plt.show()
+
+	#function to estimate the mass of Jupiter
 	def estimate_mass(self,model):
 		G_known = 6.67e-11
 		gradient = model.coef_[0]
+
+		#Using kepler's third law
 		mass_estimate = (4 * np.pi**2 * gradient) / G_known 
 
+		real_mass_jupiter = 1.898e27 #this is in kg
+		relative_error = ((mass_estimate-real_mass_jupiter)/real_mass_jupiter) * 100
+
 		print(f"Estimated Mass of Jupiter:{mass_estimate}kg")
+		print(f"Relative Error: {relative_error}%")
+
+
 
